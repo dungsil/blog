a076ff5d23a16e9df456afad961ff7ff4a45af42
김용건 (Kim Younggeon) <mail@kyg.kr>
2022-07-09 14:05:20 +0900

    fix: 레이아웃 깨짐 해결

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,24 +19,26 @@ function onClickArticle(title: string) {
     </h2>
 
     <!-- 게시글 목록 -->
-    <div v-for="path in paths" :key="path" class="divide-y divide-700 cursor-pointer" @click="onClickArticle(path)">
-      <h3 class="m0 font-900 text-4xl hover:underline">
-        {{ articles[path].title }}
-      </h3>
-      <p class="text-gray-300">
-        {{ articles[path].description ?? '' }}
-      </p>
+    <div class="space-y-8">
+      <div v-for="path in paths" :key="path" class="cursor-pointer" @click="onClickArticle(path)">
+        <h3 class="m0 font-900 text-4xl hover:underline">
+          {{ articles[path].title }}
+        </h3>
+        <p class="text-gray-300">
+          {{ articles[path].description ?? '' }}
+        </p>
 
-      <!-- 태그 목록 -->
-      <div class="flex mt4 font-light text-sm">
-        <span
-          v-for="tag in articles[path].tags" :key="tag"
-          class="ml2 first:ml0 px2 py1 border-1 rounded-xl"
-        >
-          {{ tag }}
-        </span>
+        <!-- 태그 목록 -->
+        <div class="flex mt4 font-light text-sm">
+          <span
+            v-for="tag in articles[path].tags" :key="tag"
+            class="ml2 first:ml0 px2 py1 border-1 rounded-xl"
+          >
+            {{ tag }}
+          </span>
 
-        <span class="inline-block ml-auto text-gray-200">{{ articles[path].since }}</span>
+          <span class="inline-block ml-auto text-gray-200">{{ articles[path].since }}</span>
+        </div>
       </div>
     </div>
   </div>
